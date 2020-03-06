6488ba627f78fa7e5d7861717b14ed31b46b487e
Eric Ambiel <eric.ambiel@gmail.com>
2020-03-06 15:48:35 -0300

    Implementado: Metodo para baixar contratos

--- api/routes/file.js
+++ api/routes/file.js
@@ -1,10 +1,15 @@
 var router = require('express').Router();
+const path = require('path');
 
 //TODO
-router.get('/contrato/', function(req, res, next) {
-  //res.send('Enviando arquivo!!!');
-  const file = path.join(__dirname, '../public/files/contratos/file.txt');
-  res.sendFile( file , { root: diretorio } );
+router.get('/contrato/:file', function(req, res, next) {
+  const { file } = req.params;
+
+  var dirFile = path.dirname(__dirname); //Volta um diretorio.
+  dirFile = path.join(dirFile, 'public/files/contratos'); //pasta onde os arquivos estão após API
+
+  //res.send(dirFile);
+  res.sendFile( file, { root: dirFile }); //Tratar menssagem de erro caso arquivo não seja encontrado
 });
 
 module.exports = router;
